6c1c437bb01bb72258ea00cfaac729349ab540b0
rockandrocks <patrykpesta@gmail.com>
2021-01-21 00:40:13 +0100

    Interpolacja

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'Lista zadań';
+
+// Metoda Angulara
+  getFooter(): string {
+    return '2020 © Lista zadań.';
+  }
+
+// Metoda Typescript
+  get footer2(): string {
+    return 'All rights reserved';
+  }
+}
